512cb638301c6ee01068f764fb402e4aa4d09211
Edwige08 <edwige.saves@gmail.com>
2025-03-11 11:02:06 +0100

    ajout étape 2

--- script.js
+++ script.js
@@ -22,13 +22,22 @@ const isValidDate = (dateString) => {
     }
 }
 
+const isPalindrome = (dateString) => {
+    if ((dateString.substr(0, 1) === dateString.substr(9, 1)) && (dateString.substr(1, 1) === dateString.substr(8, 1)) && (dateString.substr(3, 1) === dateString.substr(7, 1)) && (dateString.substr(4, 1) === dateString.substr(6, 1))) {
+        return true
+    }
+    return false;
+}
+
 // Tests des fonctions : --------------------------------------------
 invalidDateTestDay = "31/11/2023";
 invalidDateTestMonth = "22/22/2012";
 invalidDateTestYear = "11/02/990";
-validDateTest = "11/03/2025"
+validDateTest = "18/03/2475";
+palindromeDateTest = "22/02/2022"
 console.log("Date valide : " + isValidDate(validDateTest));
 console.log("Mauvais jour : " + isValidDate(invalidDateTestDay));
 console.log("Mauvais mois : " + isValidDate(invalidDateTestMonth));
 console.log("Mauvaise année : " + isValidDate(invalidDateTestYear));
-console.log(validDateTest.length)
+console.log("Doit retourner false : " + isPalindrome(validDateTest))
+console.log("Doit retourner true : " + isPalindrome(palindromeDateTest))
